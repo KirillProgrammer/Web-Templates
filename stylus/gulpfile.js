3736d3187c16e2574af07f0d5359c7fd9738ff58
Kirill Karpov <kirillkarpovprogramm@gmail.com>
2022-03-23 21:01:49 +0300

    add stylus template

--- stylus/gulpfile.js
+++ stylus/gulpfile.js
@@ -1,13 +1,11 @@
 const gulp         = require('gulp');
 const browserSync  = require('browser-sync');
-const sass         = require('gulp-sass')(require('sass'));
+const stylus       = require('gulp-stylus');
 const cleanCSS     = require('gulp-clean-css');
-const autoprefixer = require('autoprefixer');
+const autoprefixer = require('gulp-autoprefixer');
+const rename       = require("gulp-rename");
 const imagemin     = require('gulp-imagemin');
 const htmlmin      = require('gulp-htmlmin');
-const postcss      = require('gulp-postcss');
-const tailwindcss  = require('tailwindcss');
-const concat       = require('gulp-concat');
 
 gulp.task('server', function() {
 
@@ -21,22 +19,18 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/scss/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest("src/css"))
-        .pipe(postcss([
-            tailwindcss("./tailwind.config.js"),
-            autoprefixer(),
-        ]))
+    return gulp.src("src/stylus/**/*.styl")
+        .pipe(stylus())
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(concat({ path: 'style.css'}))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/scss/**/*.+(scss|sass|css)", gulp.parallel('styles'));
-    gulp.watch("src/*.html").on('change', gulp.parallel('html', 'styles'));
+    gulp.watch("src/stylus/**/*.(styl|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
     gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
     gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
     gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
